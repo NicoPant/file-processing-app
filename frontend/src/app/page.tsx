3ddbc282757082e5dc91dd97600dff651544ae1f
Panteleimonow <nicolas.panteleimonow@gmail.com>
2025-08-17 16:37:43 +0200

    feat: added theme for the page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,9 +3,11 @@ import { Box, Typography } from "@mui/material";
 
 export default function Home () {
     return (
-        <Box sx={ { padding: 8 } }>
-            <Typography variant={ "h3" }>File Processing Dashboard</Typography>
-            <FilesTable />
-        </Box>
+        <div>
+            <Box sx={ { padding: 8 } }>
+                <Typography variant={ "h3" }>File Processing Dashboard</Typography>
+                <FilesTable />
+            </Box>
+        </div>
     );
 }
